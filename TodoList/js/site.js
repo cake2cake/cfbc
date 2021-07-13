d6e371055724cb0b47cb791f89d742c702178fc3
cake2cake <cake2cake@users.noreply.github.com>
2021-07-13 17:00:44 +0800

    Add BS5 menu bar

--- TodoList/js/site.js
+++ TodoList/js/site.js
@@ -3,7 +3,9 @@ let count = 0;
 function addTask() {
     count++;
 
-    let TaskTemplate = `<li class="item list-group-item">Task ${count}</li>`;
+    let TaskTemplate = `<li class="item list-group-item">Task ${count} : Lorem ipsum dolor, sit amet elit!</li>`;
+    
+
     let element = document.getElementById("list");
     element.innerHTML += TaskTemplate;
     console.log(`adding task ${count}`);
